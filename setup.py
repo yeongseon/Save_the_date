501966f9f322865ffd49f928bfc0cff8e3d31e25
Yeongseon Choe <yeongseon.choe@gmail.com>
2017-11-29 00:20:48 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='save_the_date',
+    packages=['save_the_date'],
+    include_package_date=True,
+    install_requires=[
+        'flask'
+    ],
+)
