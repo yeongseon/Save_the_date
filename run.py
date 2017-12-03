e19dea02200fe207fe61b4f447272cb4695b0ba2
Yeongseon Choe <yeongseon.choe@gmail.com>
2017-12-03 13:33:43 +0900

    Add templates

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
